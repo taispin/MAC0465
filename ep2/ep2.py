cc371439e48c3754085ff662a7b877c33469011b
taispin <tais141@gmail.com>
2018-10-20 02:26:11 -0300

    gerando deltas

--- ep2/ep2.py
+++ ep2/ep2.py
@@ -15,7 +15,7 @@ def main ():
 
     else:
         #entrada = raw_input('\nInforme: r q g k e k sequencias que serão alinhadas:\n\n')
-        entrada = "2 1 0 4 atc cgga cact atc"
+        entrada = "2 1 0 3 atc cgga cact"
         parametros = ajuste_parametros(entrada)
 
         r = parametros[0]
@@ -25,6 +25,7 @@ def main ():
 
         tamanhos = []
         sequencias = []
+        deltas = []
         sequencias = list(parametros[4])
 
         print('r: '+str(r))
@@ -41,6 +42,9 @@ def main ():
         print(tamanhos)
 
         monta_matriz(k,sequencias,tamanhos)
+        v = preenche_matriz(k, tamanhos)
+        deltas = gerador_de_pontos(k)
+        print(deltas)
 
         #Agora eu tenho os parametros e sequencias.
         #Próximo passo: Como montar a matriz
@@ -116,7 +120,7 @@ def escreve_recursivo(id, sequencias, tamanhos, gravar, linhas, colunas, k, m):
         col = gravar[k-1]
         lin = gravar[0]
 
-        #nao estou conseguindo determinar o i para k = 2.
+        #Matriz funciona para k qualquer.
         #no exeplo funciona com #lin = ((cel/colunas)-1)+(gravar[0]*colunas)
         linha_maxima_1 = (linhas/(tamanhos[1]+1)) * (gravar[0]+1)
         linhas_total = linhas
@@ -130,12 +134,12 @@ def escreve_recursivo(id, sequencias, tamanhos, gravar, linhas, colunas, k, m):
 
             i = i+1
 
-        print(gravar)
+        #print(gravar)
 
         if k > 2:
             lin = lin - 1
-        print(lin)
-        print(col)
+        #print(lin)
+        #print(col)
         if(m[lin][col] == 'x'):
             m[lin][col] = list(gravar)
 
@@ -150,205 +154,60 @@ def escreve_recursivo(id, sequencias, tamanhos, gravar, linhas, colunas, k, m):
 
     return m
 
+
+def preenche_matriz(k,tamanhos):
+    v = []
+
+    #O numero de colunas eh o tamanho da ultima sequencia
+    colunas = tamanhos[k]+1
+
+    #O numero de linhas eh o produtos das demais sequencias
+    linhas = 1
+    i = 1
+    while i <= k-1:
+        linhas = linhas * (tamanhos[i]+1)
+        i = i + 1
+
+    v = ['x']*linhas
+    for i in range(linhas):
+        v[i] = ['x']*colunas
+
+    v[0][0] = 0
+
+    mostra_matriz(v)
+
+def gerador_de_pontos(k):
+    deltas = []
+
+    unidade = []
+    deltas = gerador_recursivo(1, k, deltas, unidade)
+    del deltas[0]
+
+    return deltas
+
+def gerador_recursivo(id, k, lista_delta, delta):
+
+    #base da recursão
+    if id == k+1:
+
+        lista_delta.append(list(delta))
+
+    #recursão
+    elif id <= k:
+
+        for i in range(2):
+            delta.append(i)
+            gerador_recursivo(id+1, k, lista_delta, delta)
+            lixo = delta.pop()
+
+    return lista_delta
+
 def mostra_matriz(m):
     for i in range(len(m)):
         print(m[i])
         #print('-----------')
 
-#Recebe duas sequencias s e t e retorna b e d indices para limitação
-# de alinhamento local otimo
-def encontra_limitantes(s,t):
 
-    g = -2
-    match = 1
-    mismatch = -1
-    maximo = 0
-    b = 0
-    d = 0
-
-    m = len(s) #tamanho da sequencia s
-    n = len(t) #tamanho da sequencia t
-    a = [] #lista onde calcularemos o score
-
-    for j in range(n):
-        a.append(0)
-    i = 1
-    while i < m:
-        old = a[0]
-        a[0] = 0 # punicao = 0
-        j = 1
-        while j < n:
-            temp = a[j]
-            if s[i] == t[j]:
-                p = match
-            else:
-                p = mismatch
-            a[j] = max([a[j] + g, a[j-1] + g, old+p, 0])
-            if a[j] >= maximo:
-                maximo = a[j]
-                b = i
-                d = j
-            old = temp
-            j = j + 1
-        i = i + 1
-
-    limitantes = [b,d]
-    return limitantes
-
-
-def best_score(s_orig,t_orig):
-
-    g = -2
-    match = 1
-    mismatch = -1
-
-    s = s_orig
-    t = t_orig
-
-    s.insert(0,0)
-    t.insert(0,0)
-
-    m = len(s) #tamanho da sequencia s
-    n = len(t) #tamanho da sequencia t
-    a = [] #lista onde calcularemos o score
-
-    for j in range(n):
-        a.append(j * g)
-    i = 1
-    while i < m:
-        old = a[0]
-        a[0] = i * g
-        j = 1
-        while j < n:
-            temp = a[j]
-            if s[i] == t[j]:
-                p = match
-            else:
-                p = mismatch
-            a[j] = max([a[j] + g, a[j-1] + g, old+p])
-            old = temp
-            j = j + 1
-        i = i + 1
-
-    return a
-
-def best_score_reverse(s_orig,t_orig):
-    g = -2
-    match = 1
-    mismatch = -1
-
-    s = s_orig
-    t = t_orig
-
-    s.append(0)
-    t.append(0)
-
-    m = len(s) #tamanho da sequencia s
-    n = len(t) #tamanho da sequencia t
-    a = [] #lista onde calcularemos o score
-
-    for j in range(n):
-        a.append((n-1-j) * g)
-    k = 1
-    i = m-2
-    while i >= 0:
-        old = a[n-1]
-        a[n-1] = k * g
-        k = k + 1
-        j = n-2
-        while j >= 0:
-            temp = a[j]
-            if s[i] == t[j]:
-                p = match
-            else:
-                p = mismatch
-            a[j] = max([a[j] + g, a[j+1] + g, old+p])
-            old = temp
-            j = j - 1
-        i = i - 1
-
-    return a
-
-
-def alinhamento(s,t,a,b,c,d):
-
-    tam = max([len(s),len(t)])
-    #garantimos que o alainhamento máximo vai ter no máximo 2x a maior sequencia
-    alinhamento_s = [0] * (2*tam) #alinhamento em s
-    alinhamento_t = [0] * (2*tam) #alinhamento em t
-
-    res = align(s,t,a,b,c,d,alinhamento_s,alinhamento_t)
-
-    return res
-
-def align(s,t,a,b,c,d,alin_s,alin_t):
-
-    space = '-'
-    g = -2
-
-    #se a sequencia s está vazia
-    if a > b:
-        if c <= d:
-            for i in range(d - c + 1):
-                alin_t[c+i] = s[c+i] #acertar em que indices colocar isso
-                alin_s[c+i] = "-"
-
-    #se a sequencia t está vazia
-    elif c > d:
-        if a <= b:
-            for i in range(b - a + 1):
-                alin_s[a+i] = s[a+i]
-                alin_t[a+i] = "-" #acertar onde colocar esses indices
-
-    #ambas as sequencias não são vazias
-    elif a <= b and c <= d:
-        i = (a + b)/2
-
-        pref_sim = best_score(s[a:i],t[c:d+1])
-        suff_sim = best_score_reverse(s[i+1:b+1],t[c:d+1])
-        posmax = c-1
-        typemax = 's'
-
-        #coloco um gap com s[i] e alinho t[c...d] com o que está à direita
-        vmax = pref_sim[0] + g + suff_sim[0]
-
-        j = 1
-        #procuro t[j] que quero alinhar com s[i]
-        while j <= d-c+1:
-            if (pref_sim[j-1] + p_i_j(i,j,s,t) + suff_sim[j]) > vmax:
-                posmax = j
-                typemax = 's'
-                vmax = (pref_sim[j-1] + p_i_j(i,j,s,t) + suff_sim[j])
-
-            if (pref_sim[j] + g + suff_sim[j]) > vmax:
-                posmax = j
-                typemax = 'g'
-                vamx = pref_sim[j] + g + suff_sim[j]
-
-            j = j + 1
-
-        #casei s[i] com t[j]
-        if typemax == 's':
-            align(s,t,a,i-1,c,posmax-1,alin_s,alin_t)
-            alin_s[i] = s[i]
-            alin_t[i] = t[posmax]
-            align(s,t,i+1,b,posmax+1,d,alin_s,alin_t)
-        #casei s[i] com um gap
-        else:
-            align(s,t,a,i-1,c,posmax,alin_s,alin_t)
-            alin_s[i] = s[i]
-            alin_t[i] = '-'
-            align(s,t,i+1,b,posmax+1,d,alin_s,alin_t)
-
-        return[alin_s,alin_t]
-
-def p_i_j(i,j,s,t):
-    if s[i] == t[j]:
-        p = 1
-    else:
-        p = -1
-
-    return p
 
 if __name__ == "__main__":
     main()
